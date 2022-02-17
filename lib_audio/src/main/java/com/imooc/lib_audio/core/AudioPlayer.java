b3fe76f4cda1f1b3f5f64d24e06397ba55c8d7fc
huangk <huangk@robooot.com>
2022-02-17 18:04:31 +0800

    add 音乐组件开发:1

---
+++ lib_audio/src/main/java/com/imooc/lib_audio/core/AudioPlayer.java
@@ -0,0 +1,10 @@
+package com.imooc.lib_audio.core;
+
+public class AudioPlayer {
+
+    private static final String TAG = "AudioPlayer";
+    private static final int TIME_MSG = 0x01;
+    private static final int TIME_INVAL = 100;
+
+
+}
---
+++ lib_audio/src/main/java/com/imooc/lib_audio/core/CustomMediaPlayer.java
@@ -0,0 +1,72 @@
+package com.imooc.lib_audio.core;
+
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+
+public class CustomMediaPlayer extends MediaPlayer implements MediaPlayer.OnCompletionListener {
+
+    public enum Status {
+        IDLE, INITIALIZED, STARTED, PAUSED, STOPPED, COMPLETED
+    }
+
+    private Status mState;
+
+    private OnCompletionListener mOnCompletionListener;
+
+    public CustomMediaPlayer() {
+        super();
+        mState = Status.IDLE;
+        super.setOnCompletionListener(this);
+    }
+
+    @Override
+    public void reset() {
+        super.reset();
+        mState = Status.IDLE;
+    }
+
+    @Override
+    public void setDataSource(String path) throws IOException, IllegalArgumentException, IllegalStateException, SecurityException {
+        super.setDataSource(path);
+        mState = Status.INITIALIZED;
+    }
+
+    @Override
+    public void start() throws IllegalStateException {
+        super.start();
+        mState = Status.STARTED;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        mState = Status.COMPLETED;
+        if (mOnCompletionListener != null) {
+            mOnCompletionListener.onCompletion(mp);
+        }
+    }
+
+    @Override
+    public void stop() throws IllegalStateException {
+        super.stop();
+        mState = Status.STOPPED;
+    }
+
+    @Override
+    public void pause() throws IllegalStateException {
+        super.pause();
+        mState = Status.PAUSED;
+    }
+
+    public Status getState() {
+        return mState;
+    }
+
+    public boolean isComplete() {
+        return mState == Status.COMPLETED;
+    }
+
+    public void setmOnCompletionListener(OnCompletionListener mOnCompletionListener) {
+        this.mOnCompletionListener = mOnCompletionListener;
+    }
+}
