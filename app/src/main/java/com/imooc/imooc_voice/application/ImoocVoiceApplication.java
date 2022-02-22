3a5032a9b71b2b4407463443bc9ed700aeb0a735
huangk <huangk@robooot.com>
2022-02-22 20:36:24 +0800

    add Arouter依赖

--- app/src/main/java/com/imooc/imooc_voice/application/ImoocVoiceApplication.java
+++ app/src/main/java/com/imooc/imooc_voice/application/ImoocVoiceApplication.java
@@ -2,6 +2,7 @@ package com.imooc.imooc_voice.application;
 
 import android.app.Application;
 
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.imooc.lib_audio.app.AudioHelper;
 import com.imooc.lib_share.ShareManager;
 
@@ -26,6 +27,6 @@ public class ImoocVoiceApplication extends Application {
         //更新组件下载
 //        UpdateHelper.init(this);
         //ARouter初始化
-//        ARouter.init(this);
+        ARouter.init(this);
     }
 }
